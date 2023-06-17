b2c09c82bb394c9367830326ac1c119a17c5f818
hasanaimroatun <imroatunhasana@gmail.com>
2023-06-17 10:13:22 +0700

    delete template and import Category.js

---
+++ delicious-food/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Category from './components/Category';
+
+function App() {
+  return (
+    <div className="App">
+      <Category />
+    </div>
+  );
+}
+
+export default App;
